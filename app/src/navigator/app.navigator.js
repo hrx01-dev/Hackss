56ffdbca7dfddf4defee52a5d0b734f58bfe6a46
palakgoel24 <palakgoel2023@gmail.com>
2025-12-03 00:38:10 +0530

    screenschanges

---
+++ app/src/navigator/app.navigator.js
@@ -0,0 +1,9 @@
+Import splashscreen from '..\screens\splashscreen
+Import medicinescreen from '..\\ screens\\ medicinescreen
+Import  from authscreen from '..\screens\authscreen
+Import  from chatscreen '..\screens\chatscreen
+Import  from homescreen '..\screens\homescreen
+Import insightscreen from '..\screens\insightscreen
+Import notification from '..\screens\notification
+Import onboardingscreen from '..\screens\onboardingscreen
+Import settingscreen from '..\screens\settingscreen
