63d54d1104fc9c72a1574cdb40543a5226359b10
palakgoel24 <palakgoel2023@gmail.com>
2025-12-03 11:30:47 +0530

    changes

--- app/src/navigator/app.navigator.js
+++ app/src/navigator/app.navigator.js
@@ -1,8 +1,8 @@
 Import splashscreen from '../screens/splashscreen
 Import medicinescreen from '../screens/medicinescreen
-Import  from authscreen from '../screens/authscreen
-Import  from chatscreen '../screens/chatscreen
-Import  from homescreen '../screens/homescreen
+Import  authscreen from '../screens/authscreen
+Import  chatscreen from '../screens/chatscreen
+Import  homescreen from '../screens/homescreen
 Import insightscreen from '../screens/insightscreen
 Import notification from '../screens/notification
 Import onboardingscreen from '../screens/onboardingscreen
